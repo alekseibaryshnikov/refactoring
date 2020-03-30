cb940a4bec1a3769d67de597c83c32bddb12c09f
Aleksei Baryshnikov <a250188@yandex.ru>
2020-03-30 15:25:03 +0300

    Was added tests to dependency and scaffolding for testing into project.

---
+++ src/test/java/com/ebayk/service/RatingAnalyzerTest.java
@@ -0,0 +1,11 @@
+package com.ebayk.service;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RatingAnalyzerTest {
+    @Test
+    public void getRatedUserForRatingCreator_shouldWorkCorrectlyWithValidData() {
+    }
+}
